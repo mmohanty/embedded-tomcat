2585bc677dc37cd0a497d6364689d0c17bcee354
manasmohanty <manasmohanty07@gmail.com>
2018-04-03 08:00:16 +0530

    removing unwanted code

--- src/main/java/org/manas/server/tomcat/config/SpringConfig.java
+++ src/main/java/org/manas/server/tomcat/config/SpringConfig.java
@@ -1,18 +1,9 @@
 package org.manas.server.tomcat.config;
 
-import org.manas.server.tomcat.services.UserService;
-import org.manas.server.tomcat.services.UserServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("org.manas.server.tomcat")
 public class SpringConfig {
-
-	//@Bean(name="UserServiceImpl")
-	public UserService clickCounter() {
-		return new UserServiceImpl();
-	}
-
 }
